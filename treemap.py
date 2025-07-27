150cf8e68e6d6f6cad16d58bc719848688adf84e
KasymovD <ksmv982@gmail.com>
2025-07-27 19:01:51 +0800

    Delete all files

--- treemap.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-
-file_errors_location = '../2023.xlsx'
-df = pd.read_excel(file_errors_location)
-
-fig = px.treemap(df, path=['系名稱', '國籍'])
-fig.show()
